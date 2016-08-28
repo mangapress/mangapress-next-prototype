61d013c8664c2d39c0d85214b8b86c459bcf948c
Jess Green <ardath.ksheyna78@gmail.com>
2016-08-27 23:48:00 -0500

    Adding tests

---
+++ mangapress-next.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package Manga_Press_NEXT
+ * @version $Id$
+ * @author Jessica Green <jgreen@psy-dreamer.com>
+ */
+/*
+ Plugin Name: Manga+Press NEXT Comic Manager
+ Plugin URI: http://www.manga-press.com/
+ Description: Turns WordPress into a full-featured Webcomic Manager. Be sure to visit <a href="http://www.manga-press.com/">Manga+Press</a> for more info.
+ Version: 0.0.1
+ Author: Jess Green
+ Author URI: http://www.jesgreen.com
+ Text Domain: mangapress-next
+ Domain Path: /languages
+*/
+/*
+ * (c) 2016 Jessica C Green
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
