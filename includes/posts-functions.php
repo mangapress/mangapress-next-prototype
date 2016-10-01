b45f432c33200d0baf8e686f55815dfdcf419fec
Jess Green <ardath.ksheyna78@gmail.com>
2016-10-01 17:18:19 -0500

    Updating doc-blocks

--- includes/posts-functions.php
+++ includes/posts-functions.php
@@ -131,6 +131,11 @@ function save_post($post_id, $post)
 }
 
 
+/**
+ * Set comic post terms
+ *
+ * @param int $post_id
+ */
 function set_post_terms($post_id)
 {
     // if no terms have been assigned, assign the default
@@ -149,9 +154,6 @@ function set_post_terms($post_id)
 
 /**
  * Enqueue scripts for post-edit and post-add screens
- *
- * @global WP_Post $post
- * @return void
  */
 function enqueue_scripts()
 {
@@ -190,7 +192,7 @@ function enqueue_scripts()
 /**
  * Modify header columns for Comic Post-type
  *
- * @global WP_Post $post
+ * @global \WP_Post $post
  * @param array $column
  * @return void
  */
