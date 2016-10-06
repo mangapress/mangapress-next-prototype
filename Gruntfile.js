6fce7643256b2797af359c4e9ec29a4d267ff387
Jess Green <jgreen@periscope.com>
2016-10-06 14:21:35 -0500

    Adding readme template

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,25 @@ module.exports = function (grunt) {
             options: {
                 dest: 'languages/',
                 text_domain : '<%=pkg.name%>',
-                keywords: ['__','_e','_n','_x','esc_html_e'] // functions to look for
+                keywords: [
+                    '__',
+                    '_e',
+                    '__ngettext:1,2',
+                    '_n:1,2',
+                    '__ngettext_noop:1,2',
+                    '_n_noop:1,2',
+                    '_c,_nc:4c,1,2',
+                    '_x:1,2c',
+                    '_ex:1,2c',
+                    '_nx:4c,1,2',
+                    '_nx_noop:4c,1,2',
+                    'esc_attr__',
+                    'esc_attr_e',
+                    'esc_attr_x:1,2c',
+                    'esc_html__',
+                    'esc_html_e',
+                    'esc_html_x:1,2c'
+                ] 
             },
             files: {
                 src:  [
